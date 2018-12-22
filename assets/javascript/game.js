dfe14715286614c1cf0df3010c1b76dffc7c5126
ndgithub <nickydesai.mail@gmail.com>
2018-12-22 09:23:48 -0600

    adds keyPressed validation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 var words = ["one", "two", "three"];
 var currentWordIndex = 0;
 var attempts = 0;
-var guesses = [];
+var guessed = [];
 
 function onStart() {
     document.getElementById("game_board").innerHTML = words[0].split("").map(() => "_").join("");
@@ -15,38 +15,60 @@ function logStuff(event) {
     console.log(event.key);
 }
 function onGuess(event) {
-
+    // get the key pressed
     var keyPressed = event.key;
-    guesses.push(keyPressed);
-    var gameboardWord = currentWordLetters().map(function (letter) {
-        if (guesses.indexOf(letter) === -1) {
-            return "_";
-        } else {
-            return letter;
+    if (isLetterOnly(keyPressed)) {
+
+        // If the letteronlykeyPressd is not on the guessed list.
+        if (guessed.indexOf(keyPressed) === -1) {
+            //  add the keyPressed to the guessed lit
+            guessed.push(keyPressed);
+            // create a string
+            var gameboardWord = currentWordLetters().map(function (letter) {
+                if (guessed.indexOf(letter) === -1) {
+                    return "_";
+                } else {
+                    return letter;
+                }
+            }
+            );
+            attempts++;
+            document.getElementById("guesses_remaining").innerHTML = getGuessesAllowed() - attempts;
+            document.getElementById("game_board").innerHTML = gameboardWord.join("");
+            document.getElementById("guessed").innerHTML = guessed.reduce(function (list, letter) {
+                return (list + letter.toUpperCase() + " ");
+            }, "");
+
+            if (getGuessesAllowed() - attempts === 0 || gameboardWord.indexOf("_") === -1) {
+                goToNextWord();
+            }
+
+
+            console.log("keyPressed is: " + keyPressed)
+            console.log("guesses: " + guessed);
+            console.log(gameboardWord.join(""));
         }
     }
-    );
-    attempts++;
-    document.getElementById("guesses_remaining").innerHTML = getGuessesAllowed() - attempts;
-    document.getElementById("game_board").innerHTML = gameboardWord.join("");
-    
-    if (getGuessesAllowed() - attempts === 0 || gameboardWord.indexOf("_") === -1) {
-        goToNextWord();
+}
+
+function isLetterOnly(character) {
+    if (character.length !== 1) {
+        return false;
     }
-    
-
-    console.log("keyPressed is: " + keyPressed)
-    console.log("guesses: " + guesses);
-    console.log(gameboardWord.join(""));
-
+    var checker = /^[a-z]+$/i.test(character);
+    console.log("checker is: " + checker);
+    return checker;
 
 }
 
-function goToNextWord () {
+function goToNextWord() {
     currentWordIndex++;
     attempts = 0;
     document.getElementById("guesses_remaining").innerHTML = getGuessesAllowed() - attempts;
-    guesses = [];
+    guessed = [];
+    document.getElementById("guessed").innerHTML = guessed.reduce(function (list, letter) {
+        return (list + letter.toUpperCase() + " ");
+    }, "");
     document.getElementById("game_board").innerHTML = words[currentWordIndex].split("").map(() => "_").join("");
 }
 
