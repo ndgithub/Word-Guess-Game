687afe09935f8f8804cda2cc216336b5f559a86c
ndgithub1 <nickydesai.mail@gmail.com>
2018-12-23 23:19:05 -0600

    Refactored

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var words = ["one", "two", "three"];
+var words = ["honda", "two", "three"];
 var currentWordIndex = 0;
 var attempts = 0;
 var guessed = [];
@@ -10,39 +10,49 @@ function onStart() {
     document.addEventListener("keyup", onGuess);
 }
 
-function getGuessesAllowed() {
-    return (words[currentWordIndex].length * 3);
-}
-
 function onGuess(event) {
     var keyPressed = event.key.toLowerCase();
     if (isLetterOnly(keyPressed)) {
         if (guessed.indexOf(keyPressed) === -1) {
             guessed.push(keyPressed);
-            var gameboardWord = currentWordLetters().map(function (letter) {
-                if (guessed.indexOf(letter) === -1) {
-                    return "_";
-                } else {
-                    return letter;
-                }
-            }
-            );
             attempts++;
-            document.getElementById("guesses_remaining").innerHTML = getGuessesAllowed() - attempts;
-            document.getElementById("game_board").innerHTML = gameboardWord.join(" ");
-            document.getElementById("guessed").innerHTML = guessed.reduce(function (list, letter) {
-                return (list + letter.toUpperCase() + " ");
-            }, "");
-
-            if (getGuessesAllowed() - attempts === 0) {
+            if (isRoundLost()) {
                 losses++;
                 goToNextWord();
-            } else if (gameboardWord.indexOf("_") === -1) {
+                return;
+            } else if (isRoundWon()) {
                 wins++;
                 goToNextWord();
+                return;
             }
+            updateDisplay();
         }
     }
+}
+function getGameboardWord() {
+    return (currentWordLetters().map(function (letter) {
+        if (guessed.indexOf(letter) === -1) {
+            return "_";
+        } else {
+            return letter;
+        }
+    }
+    ));
+}
+
+function goToNextWord() {
+    currentWordIndex++;
+    attempts = 0;
+    guessed = [];
+    updateDisplay();
+}
+
+function isRoundLost() {
+    return (getGuessesAllowed() - attempts === 0);
+}
+
+function isRoundWon() {
+    return (getGameboardWord().indexOf("_") === -1);
 }
 
 function isLetterOnly(character) {
@@ -53,25 +63,26 @@ function isLetterOnly(character) {
     return checker;
 }
 
-function goToNextWord() {
-    currentWordIndex++;
-    attempts = 0;
-    guessed = [];
-    updateDisplay();
-}
+
 
 function updateDisplay() {
     document.getElementById("guessed").innerHTML = guessed.reduce(function (list, letter) {
         return (list + letter.toUpperCase() + " ");
     }, "");
-    document.getElementById("game_board").innerHTML = words[currentWordIndex].split("").map(() => "_ ").join("");
+    document.getElementById("game_board").innerHTML = getGameboardWord().join("&nbsp");
     document.getElementById("wins").innerHTML = wins;
     document.getElementById("losses").innerHTML = losses;
     document.getElementById("guesses_remaining").innerHTML = getGuessesAllowed() - attempts;
 }
 
 function currentWordLetters() {
-    return (this.words[this.currentWordIndex].split(""));
+    return (words[currentWordIndex].split(""));
 }
+
+function getGuessesAllowed() {
+    return (words[currentWordIndex].length * 3);
+}
+
+
 
 onStart();
